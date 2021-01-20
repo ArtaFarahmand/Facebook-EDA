09276c0496196c58e86974b418419fa01279706a
Arta Farahmand <arta.farahmand@gmail.com>
2021-01-20 13:44:47 -0700

    Continuation of the EDA

--- Facebook-EDA.R
+++ Facebook-EDA.R
@@ -51,3 +51,44 @@ ggplot(data = fb, aes(x = dob_day)) +
   geom_histogram(bandwith = 1) +
   scale_x_continuous(breaks = 1:31) +
   facet_wrap(dob_month~gender)
+
+# Facebook friend count 
+
+ggplot(aes(x = friend_count), data = fb) + 
+  geom_histogram() + 
+  scale_x_continuous(limits = c(0,1000))
+
+# Adjusting the bin width
+
+ggplot(aes(x = friend_count), data = fb) +
+  geom_histogram(binwidth = 100) +
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0, 1000, 50)) +
+  facet_wrap(~gender)
+
+# Adjusting the the bin width and removing NA
+
+ggplot(aes(x = friend_count), data = subset(fb, !is.na(gender))) +
+  geom_histogram(binwidth = 100) +
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0, 1000, 50)) +
+  facet_wrap(~gender)
+
+# Statistics by gender
+
+table(fb$gender)
+by(fb$friend_count, fb$gender, summary)
+
+# Tenure
+
+ggplot(aes(x = tenure), data = fb) +
+  geom_histogram(binwidth = 30, color = 'black', fill = '#099DD9')
+
+# Tenure by year
+
+ggplot(aes(x = tenure / 365), data = fb) + 
+  geom_histogram(binwidth = 0.25, color = 'black', fill = '#F79420') +
+  xlab('Number of Years Using Facebook') +
+  ylab('Number of Users in Sample')
+
+# Number of likes on the web solution
+
+by(fb$www_likes, fb$gender, sum)
